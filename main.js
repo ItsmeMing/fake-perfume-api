641a580fb7c6eb3d8eda7f886fd96400bcd58d45
ItsmeMing <ngocminh.vu.520@gmail.com>
2022-11-03 10:30:59 +0700

    update PORT

--- main.js
+++ main.js
@@ -25,6 +25,6 @@ server.use((req, res, next) => {
 // Use default router
 const PORT = process.env.PORT || 3000
 server.use(router)
-server.listen(3000, () => {
+server.listen(PORT, () => {
   console.log('JSON Server is running')
 })
